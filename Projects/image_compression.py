1f694fc67aab2b2f0e418c63075e12d2d2901bc1
21K1113 <yuya.sato.2q@stu.hosei.ac.jp>
2024-11-24 21:59:21 +0900

    test

--- Projects/image_compression.py
+++ Projects/image_compression.py
@@ -166,9 +166,12 @@ def create_decoder_input_3d_v2(fp, coord, num_crops, fl, mip_level, add_noise):
         )
     decoder_input = torch.cat(decoder_input, dim=1)
     if add_noise:
-        print(decoder_input[0:4].shape)
-    decoder_input[0:4] += (torch.rand_like(decoder_input[0:4]) - 0.5) / pow(2, FP_G0_BITS)
-    decoder_input[4:5] += (torch.rand_like(decoder_input[4:5]) - 0.5) / pow(2, FP_G0_BITS)
+        fp_g0_c = FEATURE_PYRAMID_G0_CHANNELS
+        fp_g1_c = FEATURE_PYRAMID_G1_CHANNELS
+        print(decoder_input[0:4*fp_g0_c].shape)
+        print(decoder_input[4*fp_g0_c:4*fp_g0_c+fp_g1_c].shape)
+        decoder_input[0:4*fp_g0_c] += (torch.rand_like(decoder_input[0:4*fp_g0_c]) - 0.5) / pow(2, FP_G0_BITS)
+        decoder_input[4*fp_g0_c:4*fp_g0_c+fp_g1_c] += (torch.rand_like(decoder_input[4*fp_g0_c:4*fp_g0_c+fp_g1_c]) - 0.5) / pow(2, FP_G0_BITS)
     # end = time.perf_counter()
     # print(end - start)
     return decoder_input.T
