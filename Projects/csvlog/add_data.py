b6c3e40bdd2b92c00f1ccc4e63e3cf6fc138b0b0
21K1113 <yuya.sato.2q@stu.hosei.ac.jp>
2025-01-26 18:01:25 +0900

    ついでに有効桁数を決めてバッサリ切ってくれるようにした

--- Projects/csvlog/add_data.py
+++ Projects/csvlog/add_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from decimal import Decimal, ROUND_HALF_UP
 
 # CSVファイルの読み込み
 input_file = 'log.csv'  # 元のCSVファイル名
@@ -14,7 +15,7 @@ def fp_data(row):
     elif row['METHOD'] == 3 or row['METHOD'] == 4:
         fp = (row["G0 BIT"] / 8 * pow(row['IMAGE SIZE'] // pow(2, row["G0 RES"]), 3) * row["G0 CH"] +
               row["G1 BIT"] / 8 * pow(row['IMAGE SIZE'] // pow(2, row["G0 RES"] + 1), 3) * row["G1 CH"])
-    return fp
+    return int(fp)
 
 def decoder_data(row):
     h = 64
@@ -25,7 +26,29 @@ def decoder_data(row):
         decoder = ((row["G0 CH"] * 8 + row["G1 CH"] + 6 * 3 + 1 + 1) * h + (h + 1) * h + (h + 1) * 3) * b
     elif row['METHOD'] == 4:
         decoder = ((row["G0 CH"] * 4 + row["G1 CH"] + 6 * 3 + 1 + 1) * h + (h + 1) * h + (h + 1) * 3) * b
-    return decoder
+    return int(decoder)
+
+def change_data(row):
+    r = "0.001"
+    return Decimal(str(row["SUM DATA"]/1024)).quantize(Decimal(r), ROUND_HALF_UP)
+
+def change_train_time(row):
+    r = "0"
+    return Decimal(str(row["TRAIN TIME"])).quantize(Decimal(r), ROUND_HALF_UP)
+
+def change_decode_time(row):
+    # mode = "s"
+    mode = "ms"
+    r = "0.1"
+    if mode == "s":
+        decode_time = Decimal(str(row["DECODE TIME"])).quantize(Decimal(r), ROUND_HALF_UP)
+    elif mode == "ms":
+        decode_time = Decimal(str(row["DECODE TIME"]*1000)).quantize(Decimal(r), ROUND_HALF_UP)
+    return  decode_time
+
+def change_psnr(row):
+    r = "0.001"
+    return Decimal(str(row["PSNR"])).quantize(Decimal(r), ROUND_HALF_UP)
 
 
 df["FP DATA"] = df.apply(fp_data, axis=1)
@@ -45,6 +68,16 @@ df["SUM DATA"] = df["FP DATA"] + df["DECODER DATA"]
 # 'END DATE TIME' の隣に新しい列を挿入
 end_date_time_index = df.columns.get_loc('DECODER DATA') + 1
 df.insert(end_date_time_index, "SUM DATA", df.pop("SUM DATA"))
+
+df["SUM DATA (KB)"] = df.apply(change_data, axis=1)
+
+# 'END DATE TIME' の隣に新しい列を挿入
+end_date_time_index = df.columns.get_loc('SUM DATA') + 1
+df.insert(end_date_time_index, "SUM DATA (KB)", df.pop("SUM DATA (KB)"))
+
+df["TRAIN TIME"] = df.apply(change_train_time, axis=1)
+df["DECODE TIME"] = df.apply(change_decode_time, axis=1)
+df["PSNR"] = df.apply(change_psnr, axis=1)
 
 # 結果を新しいCSVファイルに保存（元のファイルを上書きしない）
 df.to_csv(output_file, index=False)
